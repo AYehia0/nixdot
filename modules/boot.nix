ff31a8c980665a7b43b7225d81acd5f32977f012
AYehia0 <ahmedyehia611@gmail.com>
2025-04-26 10:45:48 +0300

    fix(fk-nvidia): wake up from suspend fails

--- modules/boot.nix
+++ modules/boot.nix
@@ -4,5 +4,10 @@
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
+    kernelParams = [
+        "nvidia_drm.modeset=1"
+        "nvidia_drm.fbdev=1"
+        "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+      ];
   };
 }
