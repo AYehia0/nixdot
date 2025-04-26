6e54fdb0c79dd98347141518ee623f1b044f2d66
AYehia0 <ahmedyehia611@gmail.com>
2025-04-26 10:17:39 +0300

    chore(images): change wallpaper

--- modules/home/desktop/hyprland/hyprpaper.nix
+++ modules/home/desktop/hyprland/hyprpaper.nix
@@ -4,12 +4,12 @@
     settings = {
         splash = false;
         preload = [
-          "~/.dotfiles/modules/home/desktop/hyprland/wallpaper/mima-1080.png"
-          "~/.dotfiles/modules/home/desktop/hyprland/wallpaper/mima-1440.png"
+          "~/.dotfiles/modules/home/desktop/hyprland/wallpaper/whereami.jpeg"
+          "~/.dotfiles/modules/home/desktop/hyprland/wallpaper/whereami.jpeg"
         ];
         wallpaper = [
-          "eDP-1,~/.dotfiles/modules/home/desktop/hyprland/wallpaper/mima-1080.png"
-          "HDMI-A-1,~/.dotfiles/modules/home/desktop/hyprland/wallpaper/mima-1440.png"
+          "eDP-1,~/.dotfiles/modules/home/desktop/hyprland/wallpaper/whereami.jpeg"
+          "HDMI-A-1,~/.dotfiles/modules/home/desktop/hyprland/wallpaper/whereami.jpeg"
         ];
     };
   };
