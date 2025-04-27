b22a2157921bdbfdc1c32821e4d4a9cc6701b9e2
AYehia0 <ahmedyehia611@gmail.com>
2025-04-27 17:27:17 +0300

    feat(power): battery life improvements using tlp

--- modules/services.nix
+++ modules/services.nix
@@ -12,5 +12,19 @@
     };
     gvfs.enable = true;
     blueman.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        # Laptop runs hot when on power, but not on battery
+        TLP_DEFAULT_MODE = "BAT";
+        TLP_PERSISTENT_DEFAULT = 1;
+      };
+    };
   };
 }
